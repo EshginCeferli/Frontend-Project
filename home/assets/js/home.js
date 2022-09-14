3b7f25c3d38c4be8f638ebba2c9723255d04eafc
EshginCeferli <eshginij@code.edu.az>
2022-09-14 20:16:47 +0400

    Accessories page

--- home/assets/js/home.js
+++ home/assets/js/home.js
@@ -1,11 +1,19 @@
 $(document).ready(function () {
 
-    $(".language").click(function () {
-        if ($(".language-ex").hasClass('d-none')) {
-            $(".language-ex").removeClass('d-none')
-        }
-        else {
-            $(".language-ex").addClass('d-none')
+    $(".language").on("click", function () {
+        // if ($(".language-ex").hasClass('d-none')) {
+        //     $(".language-ex").removeClass('d-none')
+        // }
+        // else {
+        //     $(".language-ex").addClass('d-none')
+        // }
+        $(".language-ex").toggle();
+
+    });
+    $(document).click(function() {
+        var container = $(".language-ex");
+        if (!container.is(event.target) && !container.has(event.target).length) {
+            container.hide();
         }
     });
 
@@ -26,8 +34,66 @@ $(document).ready(function () {
             $(".profile-ex").addClass('d-none')
         }
     });
+    $('.tabmenu-wrap .tab-nav').find('a').on('click', function (e) {
+        var $this = $(this);
+        var $all_tab_nav = $this.parents('.tab-nav').find('.nav');
+        var $tab_contents = $this.parents('.tabmenu-wrap').find('.con-box');
+        var id = $this.attr('href');
+
+        e.preventDefault();
+        $all_tab_nav.removeClass('on');
+        $this.parent().addClass('on');
+        $tab_contents.hide();
+        $(id).show();
+    });
+
 })
 
-let swiper = new Swiper(".mySwiper",{
-    loop:true,
-})
+let swiper = new Swiper(".mySwiper", {
+    loop: true,
+});
+
+let swiper1 = new Swiper(".mySwiper1", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+
+    },
+});
+
+let swiper2 = new Swiper(".mySwiper2", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+let swiper3 = new Swiper(".mySwiper3", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+var swipe4 = new Swiper(".mySwiper4", {
+    slidesPerView: 4,
+    spaceBetween: 5,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+
+
+
